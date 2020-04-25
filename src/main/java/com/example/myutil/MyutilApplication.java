74e076118cd3469948c232322f94791e810987a0
xianfuhao <602807995@qq.com>
2020-04-25 16:11:30 +0800

    初始化,流的使用

---
+++ src/main/java/com/example/myutil/MyutilApplication.java
@@ -0,0 +1,13 @@
+package com.example.myutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyutilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyutilApplication.class, args);
+    }
+
+}
