eb9e3358e3e27d4103c484a9b4550983b491b963
xianfuhao <xianfh@qrkeji.net>
2020-04-26 10:02:20 +0800

    新增lombok

--- src/main/java/com/example/myutil/Util/StreamUtil.java
+++ src/main/java/com/example/myutil/Util/StreamUtil.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import lombok.Data;
 public class StreamUtil {
 
     public static void main(String[] args) {
