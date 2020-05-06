e56e3a5cd9fee020a210d0fe4b2a01687f01cf22
xianfuhao <xianfh@qrkeji.net>
2020-05-06 18:35:28 +0800

    测试类

--- src/test/java/com/example/myutil/MyutilApplicationTests.java
+++ src/test/java/com/example/myutil/MyutilApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.myutil;
 
-import org.junit.jupiter.api.Test;
+import lombok.Data;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
-class MyutilApplicationTests {
+// 下面两个注解是web环境才需要
+//@RunWith(SpringRunner.class)
+//@WebAppConfiguration
+public class MyutilApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testGetEntFileById(){
+        Assert.assertSame("企业数量有误",500,499);
     }
+
 
 }
